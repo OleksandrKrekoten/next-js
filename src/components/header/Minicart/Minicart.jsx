9f089465090a28451f215d5fda0df3951f2a7ac2
Oleksandr <98402004+OleksandrKrekoten@users.noreply.github.com>
2023-04-25 20:10:06 +0300

    fix minicart

---
+++ src/components/header/Minicart/Minicart.jsx
@@ -0,0 +1,58 @@
+import {
+  Cart,
+  CartTitle,
+  CartSubtitle,
+  LinkToCart,
+  CartList,
+  CartItem,
+  CartSum,
+  LinkToCheckout,
+  LinkToPayPal,
+} from "./Minicart.js";
+import { ImPaypal } from "react-icons/im";
+import Image from "next/image.js";
+export const Minicart = ({ isOpenCart }) => {
+  return (
+    <Cart isOpenCart={isOpenCart}>
+      <CartTitle>My Cart</CartTitle>
+      <CartSubtitle>2 item in cart</CartSubtitle>
+      <LinkToCart href="/">View or Edit Your Cart</LinkToCart>
+      <CartList>
+        <CartItem>
+          <p>1x</p>
+          <Image
+            src="/cart/cart1.jpg"
+            alt="product photo"
+            width={65}
+            height={65}
+          />
+          <h3>
+            EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...
+          </h3>
+        </CartItem>
+        <CartItem>
+          <p>1x</p>
+          <Image
+            src="/cart/cart2.jpg"
+            alt="product photo"
+            width={65}
+            height={65}
+          />
+          <h3>
+            EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...
+          </h3>
+        </CartItem>
+      </CartList>
+      <CartSum>
+        Subtotal:<span> $499.00</span>
+      </CartSum>
+      <div>
+        {" "}
+        <LinkToCheckout href="/">Go to Checkout</LinkToCheckout>
+        <LinkToPayPal href="/">
+          Check out with <ImPaypal />
+        </LinkToPayPal>
+      </div>
+    </Cart>
+  );
+};
--- src/components/header/UserMenu/UserMenu.jsx
+++ src/components/header/UserMenu/UserMenu.jsx
@@ -1,20 +1,8 @@
 import { FiSearch, FiShoppingCart } from "react-icons/fi";
-import {
-  UserWrapper,
-  SearcList,
-  Cart,
-  CartTitle,
-  CartSubtitle,
-  LinkToCart,
-  CartList,
-  CartItem,
-  CartSum,
-  LinkToCheckout,
-  LinkToPayPal
+import { UserWrapper, SearcList } from "./UserMenu.js";
 
-} from "./UserMenu.js";
-import { ImPaypal } from "react-icons/im";
 import Image from "next/image.js";
+import { Minicart } from "../Minicart/Minicart.jsx";
 const UserMenu = ({ isOpenCart, handleToggleCart }) => {
   return (
     <UserWrapper>
@@ -39,47 +27,7 @@ const UserMenu = ({ isOpenCart, handleToggleCart }) => {
           priority={true}
         />
       </div>
-      <Cart isOpenCart={isOpenCart}>
-        <CartTitle>My Cart</CartTitle>
-        <CartSubtitle>2 item in cart</CartSubtitle>
-        <LinkToCart href="/">View or Edit Your Cart</LinkToCart>
-        <CartList>
-          <CartItem>
-            <p>1x</p>
-            <Image
-              src="/cart/cart1.jpg"
-              alt="product photo"
-              width={65}
-              height={65}
-            />
-            <h3>
-              EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...
-            </h3>
-          </CartItem>
-          <CartItem>
-            <p>1x</p>
-            <Image
-              src="/cart/cart2.jpg"
-              alt="product photo"
-              width={65}
-              height={65}
-            />
-            <h3>
-              EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...
-            </h3>
-          </CartItem>
-        </CartList>
-        <CartSum>
-          Subtotal:<span> $499.00</span>
-        </CartSum>
-        <div>
-          {" "}
-          <LinkToCheckout href="/">Go to Checkout</LinkToCheckout>
-          <LinkToPayPal href="/">
-            Check out with <ImPaypal />
-          </LinkToPayPal>
-        </div>
-      </Cart>
+      <Minicart handleToggleCart={handleToggleCart} isOpenCart={isOpenCart} />
     </UserWrapper>
   );
 };
