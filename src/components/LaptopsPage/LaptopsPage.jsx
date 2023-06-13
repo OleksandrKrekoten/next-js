30d2eae736efb87baf07fd3f0ddf48633583ce9f
Oleksandr <98402004+OleksandrKrekoten@users.noreply.github.com>
2023-06-13 12:25:59 +0300

    ssr

--- src/components/LaptopsPage/LaptopsPage.jsx
+++ src/components/LaptopsPage/LaptopsPage.jsx
@@ -1,28 +1,38 @@
-import { useEffect, useState } from "react";
 import Title from "../Title/Title.jsx";
 import { Container } from "../Container";
 import ProductList from "../ProductList/ProductList.jsx";
-import api from "@/utils/api.js";
-const LaptopsPage = () => {
-    const [data, setData] = useState(null);
-    useEffect(() => {
-      (async () => {
-        try {
-          const response = await api.get("/laptops");
-          setData(response.data);
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
-      })();
-    }, []);
-   
+
+const LaptopsPage = ({ product }) => {
+ console.log(product);
+
   return (
     <Container>
       <section>
         <Title>Laptops</Title>
-        <ProductList posts={data} />
+        <ProductList posts={product} />
       </section>
     </Container>
   );
 };
+
+export async function getServerSideProps() {
+  try {
+    const res = await fetch("/api/product/laptops");
+    const data = await res.json();
+
+    return {
+      props: {
+       product: data,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      props: {
+        data: null,
+      },
+    };
+  }
+}
+
 export default LaptopsPage;
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import { Card, CardTitle,Price } from "./ProductCard.styled.js";
 
 const ProductCard = ({post}) => {
-  const {brand,model,photo,price}=post
+  console.log(post);
   return (
     <Card>
-      <Image src={photo[0]} alt="product photo" width={150} height={140} />
+      {/* <Image src={photo[0]} alt="product photo" width={150} height={140} />
       <CardTitle>{brand + " " + model}</CardTitle>
-      <Price>{price}</Price>
+      <Price>{price}</Price> */}
     </Card>
   );
 };
